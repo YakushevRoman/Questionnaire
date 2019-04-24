05beeb48ac4a03aee189c0a4c74c9b6e1e86034d
YakushevRoman <pegasus199197@gmail.com>
2019-04-24 16:31:19 +0300

    commit 15 - refactor three

--- app/src/main/java/yakushevroman1991/questionnaire/Fragments/FragmentResult.java
+++ app/src/main/java/yakushevroman1991/questionnaire/Fragments/FragmentResult.java
@@ -19,8 +19,10 @@ import yakushevroman1991.questionnaire.R;
 public class FragmentResult extends Fragment {
 
     private Button rButtonResult;
+    // for database
     private DataBaseSeller rDataBaseSeller;
     private SQLiteDatabase rSqLiteDatabase;
+    // for counting percentage
     private int count_positive = 0;
     private int count_usual = 0;
     private int count_negative = 0;
@@ -28,25 +30,31 @@ public class FragmentResult extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // create database
         rDataBaseSeller = new DataBaseSeller(getActivity());
-        rSqLiteDatabase = rDataBaseSeller.getWritableDatabase();
+        // create object for to read information
+        rSqLiteDatabase = rDataBaseSeller.getReadableDatabase();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // inflate from resurs a layout for the view
         View view = inflater.inflate(R.layout.fragment_results, container, false);
+        // push prosessing the button
         rButtonResult = view.findViewById(R.id.button_result_questionare);
         rButtonResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //
                 new GetResultQuestionary().execute();
             }
         });
         return view;
     }
-
+    // the method is  for to find the information about users
     public void getQuestionary (String name){
+        // sql the request
         String sql =
                 "Select * From "
                         + DataBaseSellerChema.Seller_TABLE.NAME + " , " + DataBaseSellerChema.INFORMATION_TABLE.NAME
@@ -54,12 +62,15 @@ public class FragmentResult extends Fragment {
                         + " And " + DataBaseSellerChema.Seller_TABLE.Columns.NAME_PEOPLE + " = '"+ name + "'"
                         + " Order by " + DataBaseSellerChema.Seller_TABLE.Columns.NAME_PEOPLE;;
 
-
+        // create cursor for the data from the database
         Cursor cursor1 = rSqLiteDatabase.rawQuery(sql,null);
         if (cursor1.moveToFirst()){
             do {
+                // get name person
                 String namePerson = cursor1.getString(cursor1.getColumnIndex(DataBaseSellerChema.Seller_TABLE.Columns.NAME_PEOPLE));
+                // get id person
                 int id = cursor1.getInt(cursor1.getColumnIndex(DataBaseSellerChema.INFORMATION_TABLE.Columns.ID));
+                // get questioning
                 int questioning = cursor1.getInt(cursor1.getColumnIndex(DataBaseSellerChema.INFORMATION_TABLE.Columns.QUESTIONNAIRE));
                 // count positive results of questioning
                 if (QuestioningConstants.QUESTIONNAIRE_HAPPY == questioning){
@@ -77,13 +88,18 @@ public class FragmentResult extends Fragment {
                 Log.d(QuestioningConstants.TAG, String.format("onClick: namePerson: %s , id: %s , questioning: %s, time: %s", namePerson, id, questioning, time));
             }while (cursor1.moveToNext());
         }
-
+        // count all results of questioning
         int rAllCount = count_positive + count_usual + count_negative;
+        // count procents positive
         double rProcentCountPositive = count_positive*100 / rAllCount;
+        // count procents usual
         double  rProcentCountUsual= count_usual*100 / rAllCount;
+        // count procents negative
         double rProcentCountNegative = count_negative*100 / rAllCount;
+        // enter log
         Log.d(QuestioningConstants.TAG, String.format("onClick: \n count positive: %s, count usual: %s, count negative: %s", count_positive, count_usual,count_negative ));
         Log.d(QuestioningConstants.TAG, String.format("onClick: \n Procent questioning : \n Positive: %s , Usual: %s, Negative: %s", rProcentCountPositive,rProcentCountUsual,rProcentCountNegative));
+        // assign
         count_positive = 0;
         count_usual = 0;
         count_negative = 0;
